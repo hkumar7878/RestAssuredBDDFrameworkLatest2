6456a4e28922429ad8865d6d8399611bc85315fb
hkumar7878 <hitesh.ghai@hotmail.com>
2018-08-17 19:38:44 +0530

    commiitting tc_05

---
+++ RestAssuredAutomation2/src/test/java/getRequestValidationLatest/TC_05.java
@@ -0,0 +1,16 @@
+package getRequestValidationLatest;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.response.ValidatableResponse;
+
+public class TC_05 {
+	
+	@Test
+	public void validTest()
+	{
+		given().get("http://services.groupkt.com/state/search/IND?text=delhi").then().statusCode(200).log().all();
+	}
+
+}
